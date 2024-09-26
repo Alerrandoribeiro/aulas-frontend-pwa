c60168159958fab3fc6b07a313145d68afb7f739
GLAUBERTY ALERRANDO CHAGAS RIBEIRO <glauberty_ribeiro@estudante.sesisenai.org.br>
2024-09-25 22:22:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Principal from './comum/componentes/Principal/Principal';
+import Rodape from './comum/componentes/Rodape/Rodape';
+import Cabecalho from './comum/componentes/cabecalho/Cabecalho';
+
+function App() {
+
+  return (
+    <>
+    <Cabecalho/>
+    <Principal/>
+    <Rodape/>
+  </>
+  );
+}
+
+export default App;
