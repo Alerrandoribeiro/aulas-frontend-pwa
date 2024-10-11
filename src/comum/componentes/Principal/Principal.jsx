c61a1751a7ace0289bd5169436f9168d7589515d
GLAUBERTY ALERRANDO CHAGAS RIBEIRO <glauberty_ribeiro@estudante.sesisenai.org.br>
2024-10-10 21:04:52 -0300

    Criando botão voltar na lista de produtos e ordenando os produtos

--- src/comum/componentes/Principal/Principal.jsx
+++ src/comum/componentes/Principal/Principal.jsx
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom';
 import BotaoCustomizado from '../BotaoCustomizado/BotaoCustomizado';
 import './Principal.css'
 
-function Principal({ titulo, children }) {
+function Principal({voltarPara, titulo, children }) {
     return (<main className="principal_root">
-        
+        {voltarPara && <Link to={voltarPara}>{'Voltar'}</Link>}
         <h1>{titulo}</h1>
 
         {children}
