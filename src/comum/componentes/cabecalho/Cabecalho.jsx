c7b3ee31da851d54eba15db4d5c62c3ae1a69fb9
GLAUBERTY ALERRANDO CHAGAS RIBEIRO <glauberty_ribeiro@estudante.sesisenai.org.br>
2024-10-23 20:15:01 -0300

    subindo pwa

--- src/comum/componentes/cabecalho/Cabecalho.jsx
+++ src/comum/componentes/cabecalho/Cabecalho.jsx
@@ -5,7 +5,7 @@ import './Cabecalho.css'
 function Cabecalho(){
     return <header className="cabecalho_root"> 
     <img src ="/vite.svg" height={40} />
-    <Avatar nome = "José Silva" />
+    <Avatar nome = "Glauberty Ribeiro" />
 
      </header>
 }
